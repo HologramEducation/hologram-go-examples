e66e51f000c0d134d6f03e99aefa54fea2edf8a1
Zheng Hao Tan <tanzhao@umich.edu>
2016-07-01 10:44:36 -0500

    added devices.go unit test

---
+++ Device/Devices.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "../../Hologram-Go"
+    "fmt"
+)
+
+func main() {
+
+    HologramGo.InitializeUsernameAndPassword("../credentials.json")
+
+    fmt.Println("Welcome to the GetDevices example")
+
+    var devices = HologramGo.GetDevices()
+
+    device := (HologramGo.Device)(devices[0].(map[string]interface{}))
+
+    fmt.Print("Device ID: ")
+    fmt.Println(device.GetDeviceId())
+
+    fmt.Print("User ID: ")
+    fmt.Println(device.GetDeviceUserId())
+
+    fmt.Println("Device Name: " + device.GetDeviceName())
+
+    fmt.Println("Device Type: " + device.GetDeviceType())
+
+    fmt.Println("Device Created: " + device.GetWhenCreated())
+}
+
+// Example output:
+
+// Welcome to the GetDevices example
+// Device ID: 84
+// User ID: 4477
+// Device Name: Unnamed Device (90123)
+// Device Type: Unknown
+// Device Created: 2016-06-06 21:51:16
